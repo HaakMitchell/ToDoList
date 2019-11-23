e01dda5dfa7e903ae3dadc029abe28fd324c7a6b
Mitchell Haak <55114499+HaakMitchell@users.noreply.github.com>
2019-11-22 23:39:42 -0500

    Add files via upload

---
+++ Assignment-Two/main.js
@@ -0,0 +1,14 @@
+let btn = document.getElementById('add');
+btn.addEventListener('click', function() {
+
+  var check = document.createElement('input');
+  check.type = 'checkbox';
+  addEventListener('click', function() {
+
+  });
+
+  var para = document.createElement('P');
+  para.innerHTML = '<input type="checkbox">' + document.getElementById('task').value
+  document.getElementById('list').appendChild(para);
+
+});
